44b106c1332afe3c947608fd1880fd9c3c15deae
GitHub Action <action@github.com>
2021-06-08 14:43:29 +0000

    Template cleanup

--- src/main/kotlin/com/github/hakubjozak/fileswitcher/services/MyApplicationService.kt
+++ src/main/kotlin/com/github/hakubjozak/fileswitcher/services/MyApplicationService.kt
@@ -1,6 +1,6 @@
-package org.jetbrains.plugins.template.services
+package com.github.hakubjozak.fileswitcher.services
 
-import org.jetbrains.plugins.template.MyBundle
+import com.github.hakubjozak.fileswitcher.MyBundle
 
 class MyApplicationService {
 
